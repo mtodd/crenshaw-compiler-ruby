5e183c264411d6963acce3841b274fdd7c78c807
Matt Todd <chiology@gmail.com>
2016-01-01 13:06:18 -0800

    Read multi-digit numbers

--- compiler-part4.rb
+++ compiler-part4.rb
@@ -100,13 +100,16 @@ end
 #
 # Returns the digit character String.
 def get_num
-  la = $lookahead
+  value = 0
 
-  return expected("Integer") unless is_digit(la)
+  return expected("Integer") unless is_digit($lookahead)
 
-  lookahead
+  while is_digit($lookahead)
+    value = 10 * value + $lookahead.to_i
+    lookahead
+  end
 
-  la.to_i
+  value
 end
 
 # Internal: Output a String with Tab
